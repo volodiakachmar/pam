74e2b543f6ec1d3adc293762f32303eb7c6a1b75
Volodymyr Kachmar <37980324+volodiakachmar@users.noreply.github.com>
2021-05-16 21:01:54 +0200

    adding rooms

---
+++ src/navigation/HomeStack.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+import AddRoomScreen from '../screens/AddRoomScreen';
+import { IconButton } from 'react-native-paper';
+
+const ChatAppStack = createStackNavigator();
+const ModalStack = createStackNavigator();
+
+
+function ChatApp() {
+  return (
+    <ChatAppStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: '#6646ee'
+        },
+        headerTintColor: '#ffffff',
+        headerTitleStyle: {
+          fontSize: 22
+        }
+      }}
+    >
+      <ChatAppStack.Screen
+  name="Home"
+  component={HomeScreen}
+  options={({ navigation }) => ({
+    headerRight: () => (
+      <IconButton
+        icon="message-plus"
+        size={28}
+        color="#ffffff"
+        onPress={() => navigation.navigate('AddRoom')}
+      />
+    )
+  })}
+/>
+    </ChatAppStack.Navigator>
+  );
+}
+
+export default function HomeStack() {
+  return (
+    <ModalStack.Navigator mode="modal" headerMode="none">
+      <ModalStack.Screen name="ChatApp" component={ChatApp} />
+      <ModalStack.Screen name="AddRoom" component={AddRoomScreen} />
+    </ModalStack.Navigator>
+  );
+}
